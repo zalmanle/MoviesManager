923b8706df4e75864eeaa64f69c9addc61519faa
zalmanle <zalmanle@gmail.com>
2016-03-15 16:52:12 +0200

    //add classes for interaction with user//

--- app/src/main/java/com/example/user/moviesmanager/info/MoviesUserInfo.java
+++ app/src/main/java/com/example/user/moviesmanager/info/MoviesUserInfo.java
@@ -3,7 +3,6 @@ package com.example.user.moviesmanager.info;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
-import android.widget.ImageView;
 
 import com.example.user.moviesmanager.R;
 
@@ -17,7 +16,7 @@ public class MoviesUserInfo extends UserInfo {
     }
 
     //region SHOW ALERT DIALOG ON LIST ITEM CLICKED
-    public void showMovieOptionDialog(final int position, final ImageView imageView, DialogInterface.OnClickListener listener) {
+    public void showMovieOptionDialog(DialogInterface.OnClickListener listener) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.options_dialog_title);
         builder.setItems(R.array.movie_options_items, listener);
@@ -27,7 +26,7 @@ public class MoviesUserInfo extends UserInfo {
     //endregion
 
     //region SHOW ALERT DIALOG TO REMOVE ITEM
-    private void showDeleteItemWarning(final int position,DialogInterface.OnClickListener listener) {
+    public void showDeleteItemWarning(DialogInterface.OnClickListener listener) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.warning_title_text);
         builder.setIcon(R.drawable.red_warning_icon);
@@ -45,7 +44,7 @@ public class MoviesUserInfo extends UserInfo {
     }
     //endregion
     //region SHOW ALERT DIALOG TO REMOVE ALL ITEMS
-    private void showDeleteAllItemsWarning(DialogInterface.OnClickListener listener) {
+    public void showDeleteAllItemsWarning(DialogInterface.OnClickListener listener) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.warning_title_text);
         builder.setIcon(R.drawable.red_warning_icon);
